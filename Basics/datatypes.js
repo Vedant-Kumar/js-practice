3a0956140bd5afcbc316fe3b2e12c8d9151d52e5
Vedant-Kumar <42078597+Vedant-Kumar@users.noreply.github.com>
2025-11-11 01:11:22 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let name = "Vedant";
+let age = 26;
+let isLoggedin = true;
+
+// null 
+// undefined
+// symbol => unique identifier
+
+// object
+
+console.log(typeof age);
---
+++ Basics/variables.js
@@ -0,0 +1,10 @@
+const personName = "Vedant Kumar";
+let profession = "Student";
+var age = 26;
+
+console.table([personName, profession, age]);
+
+
+
+
+
