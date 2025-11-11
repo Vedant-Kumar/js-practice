b0336f31ea508454af223dfb8a3f4d750bfcdaa3
Vedant-Kumar <42078597+Vedant-Kumar@users.noreply.github.com>
2025-11-11 01:37:11 +0000

    Type conversion #

---
+++ Basics/typeconversion.js
@@ -0,0 +1,20 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof (valueInNumber));
+
+
+let isLoggedin = 1;
+console.log(typeof isLoggedin);
+
+let booleanIsLoggedin = Boolean(isLoggedin);
+console.log(typeof booleanIsLoggedin);
+
+
+let someNumber = 34;    
+stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
