059847ea73f9c87880ba08d53486c9e9c148104b
Vedant-Kumar <42078597+Vedant-Kumar@users.noreply.github.com>
2026-09-14 00:56:31 +0000

    Math Object (mathematical functions)

--- Basics/basicprac.js
+++ Basics/basicprac.js
@@ -164,3 +164,30 @@
 
 // let gameName = new String("Call-Of-Duty");
 // console.log(gameName.split("-"));
+
+// const score = 100;
+// const balance = new Number(100);
+// console.log(score === balance);
+// console.log(score == balance);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.8966;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.1));
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(2,3));
+// console.log(Math.min(0, 150, 30, 20, -8, -200));
+// console.log(Math.max(0, 150, 30, 20, -8, -200));
+// console.log(Math.random());
+// console. log (Math.floor(Math. random() * (max - min + 1)) + min)
+// console. log (Math. floor(Math. random()*10)+1);
+
